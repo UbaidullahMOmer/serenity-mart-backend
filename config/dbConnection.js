faae53b22ba836a27248c35c28c00bca6f4c92a3
Ubaidullah Omer <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-09-07 19:34:32 +0500

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,16 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect("mongodb+srv://ubaidullahmomer:TMUlFP0xwtahxB37@ubaidullahcluster.hdqsh.mongodb.net/serenity-mart?retryWrites=true&w=majority&appName=ubaidullahcluster", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const connect = await mongoose.connect("mongodb+srv://ubaidullahmomer:TMUlFP0xwtahxB37@ubaidullahcluster.hdqsh.mongodb.net/serenity-mart?retryWrites=true&w=majority&appName=ubaidullahcluster");
     console.log(
       `MongoDB connected: ${connect.connection.host}; Database: ${connect.connection.name}`
     );
   } catch (err) {
     console.error(`Error: ${err.message}`);
-    // To log stack trace for further debugging
     console.error(err.stack);
     process.exit(1);
   }
