bd2197dd8ec429b8eacc959ab211a4b72d911922
Ubaidullah Omer <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-09-14 18:05:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,23 +8,32 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
+// CORS configuration
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://serenity-mart-backend.vercel.app/",
-      "https://www.serenitymartpk.com/",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+      "https://serenity-mart-backend.vercel.app",
+      "https://www.serenitymartpk.com",
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS policy violation'));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Serenity Mart Backend API");
 });
