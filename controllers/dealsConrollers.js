cb20e7b4b321043d3bff5aa941f4585503d18f0c
Ubaidullah Omer <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-09-17 23:32:08 +0500

    update

--- controllers/dealsConrollers.js
+++ controllers/dealsConrollers.js
@@ -5,9 +5,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: "daqzyx2uv",
+  api_key: "645274696555577",
+  api_secret: "TtGTW3BvC4wcyOSFwrrTWGRmRsY"
 });
 
 const storage = new CloudinaryStorage({
--- controllers/heroControllers.js
+++ controllers/heroControllers.js
@@ -4,10 +4,10 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({ 
+  cloud_name: "daqzyx2uv",
+  api_key: "645274696555577",
+  api_secret: "TtGTW3BvC4wcyOSFwrrTWGRmRsY"
 });
 
 const storage = new CloudinaryStorage({
